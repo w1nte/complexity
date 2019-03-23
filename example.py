32e953cdc2d37fc287cdd990ae5f50f3b98c92b1
w1nte <miwel33@gmail.com>
2019-03-23 13:50:31 +0100

    Change example function

--- example.py
+++ example.py
@@ -1,24 +1,34 @@
 import complexity
 
+import random
+
 
 @complexity.test
-def algodat_01_5b(n):
+def selectionsort(n, shuffle=False):
+
     c = 1  # cost of the function
 
-    i = 1
-    x = 1
+    li = list(range(n, 0, -1))
 
-    while i < n:
-        a = 4*i
-        for j in range(int(a), 1, -1):
-            x = x+j
+    if shuffle:
+        random.shuffle(li)
+
+    for i, x in enumerate(li[:-1]):
+        min = i
+        for j, y in enumerate(li[i+1:]):
             c += 1
-        i = a/2
+            if y < li[min]:
+                min = i+j+1
+        if min is not i:
+            li[i], li[min] = li[min], li[i]
+
         c += 1
 
-    return c  # returns the costs
+    return c
 
+
+complexity.testrange = [10, 100, 1000]
 
 if __name__ == '__main__':
 
-    algodat_01_5b()
+    selectionsort(shuffle=True)
